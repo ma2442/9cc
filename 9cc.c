83118a7361846dfe86fb5a887f95d2c3beaefe0c
ma <username.seimei0@gmail.com>
2021-08-11 02:49:49 +0900

    エラー箇所を指し示す機能追加

--- 9cc.c
+++ 9cc.c
@@ -6,7 +6,8 @@
 #include <string.h>
 
 // トークンの種類
-typedef enum {
+typedef enum
+{
     TK_RESERVED, // 記号
     TK_NUM,      // 整数トークン
     TK_EOF,      // 入力の終わりを表すトークン
@@ -15,7 +16,8 @@ typedef enum {
 typedef struct Token Token;
 
 // トークン型
-struct Token {
+struct Token
+{
     TokenKind kind; // トークンの型
     Token *next;    // 次の入力トークン
     int val;        // kindがTK_NUMの場合、その数値
@@ -25,10 +27,28 @@ struct Token {
 // 現在着目しているトークン
 Token *token;
 
+// 入力プログラム
+char *user_input;
+
+// エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " "); // pos個の空白を出力
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
 
 // エラーを報告するための関数
 // printfと同じ引数を取る
-void error(char *fmt, ...) {
+void error(char *fmt, ...)
+{
     va_list ap;
     va_start(ap, fmt);
     vfprintf(stderr, fmt, ap);
@@ -36,49 +56,52 @@ void error(char *fmt, ...) {
     exit(1);
 }
 
-
 // 次のトークンが期待している記号のときには、トークンを1つ読み進めて
 // 真を返す。それ以外の場合には偽を返す。
-bool consume(char op){
-//  input your answer
-    if ( token->kind != TK_RESERVED || token->str[0] != op ) {
+bool consume(char op)
+{
+    //  input your answer
+    if (token->kind != TK_RESERVED || token->str[0] != op)
+    {
         return false;
     }
     token = token->next;
     return true;
 }
 
-
 // 次のトークンが期待している記号のときには、トークンを1つ読み進める。
 // それ以外の場合にはエラーを報告する。
-void expect(char op) {
-    if ( token->kind != TK_RESERVED || token->str[0] != op ) {
-        error("'%c'ではありません", op);
+void expect(char op)
+{
+    if (token->kind != TK_RESERVED || token->str[0] != op)
+    {
+        error_at(token->str, "'%c'ではありません", op);
     }
     token = token->next;
     return;
 }
 
-
 // 次のトークンが数値の場合、トークンを1つ読み進めてその数値を返す。
 // それ以外の場合にはエラーを報告する。
-int expect_number() {
-    if (token->kind != TK_NUM) {
-        error("数ではありません");
+int expect_number()
+{
+    if (token->kind != TK_NUM)
+    {
+        error_at(token->str, "数ではありません");
     }
     int val = token->val;
     token = token->next;
     return val;
 }
 
-
-bool at_eof() {
-  return token->kind == TK_EOF;
+bool at_eof()
+{
+    return token->kind == TK_EOF;
 }
 
-
 // 新しいトークンを作成してcurに繋げる
-Token *new_token(TokenKind kind, Token *cur, char *str) {
+Token *new_token(TokenKind kind, Token *cur, char *str)
+{
     Token *tok = calloc(1, sizeof(Token));
     tok->kind = kind;
     tok->str = str;
@@ -86,47 +109,52 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
     return tok;
 }
 
-
 // 入力文字列pをトークナイズしてそれを返す
-Token *tokenize(char *p) {
+Token *tokenize(char *p)
+{
     Token head;
     head.next = NULL;
     Token *cur = &head;
 
-    while(*p) {
+    while (*p)
+    {
 
-        if (isspace(*p)) {
+        if (isspace(*p))
+        {
             p++;
             continue;
         }
 
-        if (*p == '+' || *p == '-') {
+        if (*p == '+' || *p == '-')
+        {
             cur = new_token(TK_RESERVED, cur, p);
             p++;
             continue;
         }
 
-        if (isdigit(*p)) {
+        if (isdigit(*p))
+        {
             cur = new_token(TK_NUM, cur, p);
             cur->val = strtol(p, &p, 10);
             continue;
         }
 
-        error("トークナイズできません");
+        error_at(p, "トークナイズできません");
     }
 
     new_token(TK_EOF, cur, p);
     return head.next;
 }
 
-
-
-
-int main (int argc, char **argv) {
-    if (argc != 2) {
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
         error("引数の個数が正しくありません");
         return 1;
     }
+
+    user_input = argv[1];
 
     // トークナイズする
     token = tokenize(argv[1]);
@@ -142,8 +170,10 @@ int main (int argc, char **argv) {
 
     // `+ <数>` あるいは `-<数>`というトークンの並びを消費しつつ
     // アセンブリを出力
-    while (!at_eof()) {
-        if (consume('+')) {
+    while (!at_eof())
+    {
+        if (consume('+'))
+        {
             printf("  add rax, %d\n", expect_number());
             continue;
         }
@@ -155,4 +185,3 @@ int main (int argc, char **argv) {
     printf("  ret\n");
     return 0;
 }
-
